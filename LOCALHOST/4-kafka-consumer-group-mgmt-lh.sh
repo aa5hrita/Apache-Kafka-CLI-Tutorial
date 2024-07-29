b7056aba9c9407d7d96b34059e29968c1a51753d
AASHRITA BILIANGADI <aashrita.biliangadi@gmail.com>
2024-07-30 00:21:30 +0530

    done

--- LOCALHOST/4-kafka-consumer-group-mgmt-lh.sh
+++ LOCALHOST/4-kafka-consumer-group-mgmt-lh.sh
@@ -1,32 +1,42 @@
-
 ############################
 #####     LOCALHOST    #####
 ############################
-
 
 # documentation for the command 
 kafka-consumer-groups.sh 
 
 # list consumer groups
-kafka-consumer-groups.sh --bootstrap-server localhost:9092 --list
+aashritakafka@DESKTOP-U2SQV3E:~$ kafka-consumer-groups.sh --bootstrap-server localhost:9092 --list
+AAA
+my-first-application
+myConsumerApp
  
-# describe one specific group
-kafka-consumer-groups.sh --bootstrap-server localhost:9092 --describe --group my-second-application
+# describe one specific consumer group
+aashritakafka@DESKTOP-U2SQV3E:~$ kafka-consumer-groups.sh --bootstrap-server localhost:9092 --describe --group my-first-application
 
-# describe another group
-kafka-consumer-groups.sh --bootstrap-server localhost:9092 --describe --group my-first-application
+Consumer group 'my-first-application' has no active members.
 
-# start a consumer
-kafka-console-consumer.sh --bootstrap-server localhost:9092 --topic first_topic --group my-first-application
+GROUP                TOPIC           PARTITION  CURRENT-OFFSET  LOG-END-OFFSET  LAG             CONSUMER-ID     HOST            CLIENT-ID
+my-first-application second_topic    3          67              67              0               -               -               -
+my-first-application second_topic    0          24              24              0               -               -               -
+my-first-application second_topic    2          2               2               0               -               -               -
+my-first-application second_topic    1          3               3               0               -               -               -
+my-first-application second_topic    4          9               9               0               -               -               -
+#if there are msgs produced but not consumed and commmiteed then  LOG-END-OFFset value will read
+# a higher value and the LAG will be the difference of CURR-OFFSET and LOG-END-OFFSET
 
-# describe the group now
-kafka-consumer-groups.sh --bootstrap-server localhost:9092 --describe --group my-first-application
+# start 2 consumers in 2 windows - MFA: C1 - reading P0&P1 and C2 reading P2
+aashritakafka@DESKTOP-U2SQV3E:~$ kafka-console-consumer.sh --bootstrap-server localhost:9092 --topic first_topic --group my-first-application
+aashritakafka@DESKTOP-U2SQV3E:~$ kafka-console-consumer.sh --bootstrap-server localhost:9092 --topic first_topic --group my-first-application
 
-# describe a console consumer group (change the end number)
-kafka-consumer-groups.sh --bootstrap-server localhost:9092 --describe --group console-consumer-10592
+# describe the group now to see different consumers reading different partitions of same topic
+aashritakafka@DESKTOP-U2SQV3E:~$ kafka-consumer-groups.sh --bootstrap-server localhost:9092 --describe --group my-first-application
 
-# start a console consumer
-kafka-console-consumer.sh --bootstrap-server localhost:9092 --topic first_topic --group my-first-application
+# start a console consumer - AK puts it in default CGroup
+aashritakafka@DESKTOP-U2SQV3E:~$ kafka-console-consumer.sh --bootstrap-server localhost:9092 --topic first_topic --group my-first-application
+
+# list to know available consumers in default group describe a console consumer group (change the end number)
+aashritakafka@DESKTOP-U2SQV3E:~$ kafka-consumer-groups.sh --bootstrap-server localhost:9092 --describe --group console-consumer-10592
 
 # describe the group again
 kafka-consumer-groups.sh --bootstrap-server localhost:9092 --describe --group my-first-application
--- LOCALHOST/5-reset-offsets-lh.sh
+++ LOCALHOST/5-reset-offsets-lh.sh
@@ -1,34 +1,3 @@
-# Replace "kafka-consumer-groups" 
-# by "kafka-consumer-groups.sh" or "kafka-consumer-groups.bat" based on your system # (or bin/kafka-consumer-groups.sh or bin\windows\kafka-consumer-groups.bat if you didn't setup PATH / Environment variables)
-
-
-############################
-### CONDUKTOR PLATFORM #####
-############################ 
-
-# look at the documentation again
-kafka-consumer-groups.sh
-
-# describe the consumer group
-kafka-consumer-groups.sh --command-config playground.config --bootstrap-server cluster.playground.cdkt.io:9092 --describe --group my-first-application
-
-# Dry Run: reset the offsets to the beginning of each partition
-kafka-consumer-groups.sh --command-config playground.config --bootstrap-server cluster.playground.cdkt.io:9092 --group my-first-application --reset-offsets --to-earliest --topic third_topic --dry-run
-
-# execute flag is needed
-kafka-consumer-groups.sh --command-config playground.config --bootstrap-server cluster.playground.cdkt.io:9092 --group my-first-application --reset-offsets --to-earliest --topic third_topic --execute
-
-# describe the consumer group again
-kafka-consumer-groups.sh --command-config playground.config --bootstrap-server cluster.playground.cdkt.io:9092 --describe --group my-first-application
-
-# consume from where the offsets have been reset
-kafka-console-consumer.sh --consumer.config playground.config --bootstrap-server cluster.playground.cdkt.io:9092 --topic third_topic --group my-first-application
-
-# describe the group again
-kafka-consumer-groups.sh --command-config playground.config --bootstrap-server cluster.playground.cdkt.io:9092 --describe --group my-first-application
-
-
-
 ############################
 #####     LOCALHOST    #####
 ############################
@@ -38,6 +7,10 @@ kafka-consumer-groups.sh
 
 # describe the consumer group
 kafka-consumer-groups.sh --bootstrap-server localhost:9092 --describe --group my-first-application
+
+<You cannot reset a consumer group if consumers are active in it>
+<The CURRENT-OFFSET is the latest committed offset for that group>
+<The LOG-END-OFFSET represents the latest message offset available in the topic-partition for consumption>
 
 # Dry Run: reset the offsets to the beginning of each partition
 kafka-consumer-groups.sh --bootstrap-server localhost:9092 --group my-first-application --reset-offsets --to-earliest --topic third_topic --dry-run
@@ -53,3 +26,23 @@ kafka-console-consumer.sh --bootstrap-server localhost:9092 --topic third_topic 
 
 # describe the group again
 kafka-consumer-groups.sh --bootstrap-server localhost:9092 --describe --group my-first-application
+
+#Listing consumer groups state
+aashritakafka@DESKTOP-U2SQV3E:~$ kafka-consumer-groups.sh --bootstrap-server localhost:9092 --list --state
+GROUP                STATE
+AAA                  Empty
+my-first-application Empty
+myConsumerApp        Empty
+
+#Describe all consumer groups and state 
+aashritakafka@DESKTOP-U2SQV3E:~$ kafka-consumer-groups.sh --bootstrap-server localhost:9092 --describe --all-groups  --state
+
+#Deletion requested for a consumer group
+aashritakafka@DESKTOP-U2SQV3E:~$ kafka-consumer-groups.sh --bootstrap-server localhost:9092 --delete --group my-first-application
+Deletion of requested consumer groups ('my-first-application') was successful.
+
+# delete offsets for a specific topic (helpful when your consumer group is reading from multiple topics) you can use the following command:>
+# Perform operations on the consumer offsets using kafka-consumer-groups
+kafka-consumer-groups.sh --bootstrap-server localhost:9092 --delete-offsets --group my-first-application --topic first_topic
+<reads from first msg next time when a consumer is started>
+<to reset - the consumers must be stopped>
